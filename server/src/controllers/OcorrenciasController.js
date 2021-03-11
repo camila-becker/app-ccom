43e78512a2658fedcae6bd8e73700bcd662e8a14
Camila Becker <39452095+camila-becker@users.noreply.github.com>
2021-03-11 09:02:07 -0300

    Adicionado email

--- server/src/controllers/OcorrenciasController.js
+++ server/src/controllers/OcorrenciasController.js
@@ -110,7 +110,7 @@ module.exports = {
           from: "ccom.ocorrencias@gmail.com",
           to: `${emailPorFilial(origem)}`,
           cc:
-            "ccom.controle@modular.com.br, ccom.gestao@modular.com.br, giulian@modular.com.br, jessica.maiser@modular.com.br, alberi.silva@modular.com.br",
+            "ccom.controle@modular.com.br, ccom.gestao@modular.com.br, giulian@modular.com.br, jessica.maiser@modular.com.br, alberi.silva@modular.com.br, rice@modular.com.br",
           subject: `Registro de Ocorrência - ${motivo} ${placa} / ${origem}`,
           text: "",
           html: output,
